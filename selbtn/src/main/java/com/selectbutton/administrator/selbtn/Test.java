923b0085e2a72fcbcbbb41158ea638b1a1f3c11b
zhangyu <18842848897@163.com>
2018-03-02 13:04:00 +0800

    init

---
+++ selbtn/src/main/java/com/selectbutton/administrator/selbtn/Test.java
@@ -0,0 +1,15 @@
+package com.selectbutton.administrator.selbtn;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by zy on 2018/3/2.
+ * Describe 描述
+ */
+
+public class Test {
+    public static void ToastTest(Context context){
+        Toast.makeText(context, "test", Toast.LENGTH_SHORT).show();
+    }
+}
